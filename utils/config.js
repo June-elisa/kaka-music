dc8aa191a5f1a1b610beb66c931d9b82b896906d
xuelianYi <1315126511@qq.com>
2021-08-18 14:26:14 +0800

    完成首页基本界面

---
+++ utils/config.js
@@ -0,0 +1,12 @@
+/*
+ * @Desc: 配置服务器相关信息
+ * @Author: xuelianYi
+ * @Date: 2021-08-16 17:17:30
+ * @LastEditors: xuelianYi
+ * @LastEditTime: 2021-08-18 12:06:21
+ * @FilePath: \kaka_music\utils\config.js
+ */
+export default {
+    host: 'http://localhost:3000',
+    mobileHost: 'https://da274289cb75.ngrok.io'
+}
