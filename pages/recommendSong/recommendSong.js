180d14df6f6d8f59cef092b5265649e861d13002
xuelianYi <1315126511@qq.com>
2021-08-31 18:46:13 +0800

    每日推荐样式

---
+++ pages/recommendSong/recommendSong.js
@@ -0,0 +1,98 @@
+/*
+ * @Desc: 
+ * @Author: xuelianYi
+ * @Date: 2021-08-31 11:11:57
+ * @LastEditors: xuelianYi
+ * @LastEditTime: 2021-08-31 18:44:48
+ * @FilePath: \kaka_music\pages\recommendSong\recommendSong.js
+ */
+// pages/recommendSong/recommendSong.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    day: '', // 天
+    month: '', // 月
+    recommendList: [], // 推荐列表数据
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    // 判断用户是否登录
+    let userInfo = wx.getStorageSync('userInfo');
+    if (!userInfo) {
+      wx.showToast({
+        title: '请先登录',
+        icon: 'none',
+        success: () => {
+          // 跳转至登录界面
+          wx.reLaunch({
+            url: '/pages'
+          })
+        }
+      })
+    }
+
+
+
+    // 更新日期的状态数据
+    this.setData({
+      day: new Date().getDate(),
+      month: new Date().getMonth() + 1
+    })
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
